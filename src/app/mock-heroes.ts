1fc151e3470145a4bf22732d20c3558721b20307
Nikolay Tonchev <ntonchev.89@outlook.com>
2022-04-17 19:07:32 +0300

    Second section completed

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr Nice', power: 'Being nice' },
+    { id: 12, name: 'Narco', power: 'Ccccocaine' },
+    { id: 13, name: 'Bombasto', power: 'Blows everything' },
+    { id: 14, name: 'Celeritas', power: 'Eats celery' },
+    { id: 15, name: 'Magneta', power: 'Plays with magnets' },
+    { id: 16, name: 'RubberMan', power: 'Uses protection ;)' },
+    { id: 17, name: 'Dynama', power: 'Pyromaniac' },
+    { id: 18, name: 'Dr IQ', power: 'Smarty-pants' },
+    { id: 19, name: 'Magma', power: 'Hot hot hot' },
+    { id: 20, name: 'Tornado', power: 'Takes away your house, car, money...' }
+]
